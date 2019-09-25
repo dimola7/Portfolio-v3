3554422fc90f12fc1ecbd77ddf4e00c23096b9d4
adedimola <dimola.ogidan@multistreamtechnologies.com>
2019-09-25 17:07:29 +0100

    initialize portfolio

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()));
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+};
+
+// var btn = $('#contact');
+
+// $(window).scroll(function() {
+//   if ($(window).scrollTop() > 300) {
+//     btn.addClass('show');
+//   } else {
+//     btn.removeClass('show');
+//   }
+// });
+
+// btn.on('click', function(e) {
+//   e.preventDefault();
+//   $('html, body').animate({scrollTop:0}, '300');
+// });
+
+
