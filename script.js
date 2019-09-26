fb47927ddd4095947ad005d30ed5b6581997b583
adedimola <dimola.ogidan@multistreamtechnologies.com>
2019-09-26 13:17:42 +0100

    clean up code

--- script.js
+++ script.js
@@ -19,19 +19,5 @@ function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 };
 
-// var btn = $('#contact');
-
-// $(window).scroll(function() {
-//   if ($(window).scrollTop() > 300) {
-//     btn.addClass('show');
-//   } else {
-//     btn.removeClass('show');
-//   }
-// });
-
-// btn.on('click', function(e) {
-//   e.preventDefault();
-//   $('html, body').animate({scrollTop:0}, '300');
-// });
 
 
