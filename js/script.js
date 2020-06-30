33d3502208841109617fd6657a42876be6c28c6a
Dimola Ogidan <didiogidan@gmail.com>
2020-06-30 15:54:13 +0100

    changed theme

---
+++ js/script.js
@@ -0,0 +1,53 @@
+document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()));
+let t1 = new TimelineLite();
+let btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300'); 
+});
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+};
+
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".skills",
+  triggerHook: 0,
+})
+  .setTween(t1)
+  .addTo(controller);
+
+const showRequiredCategory = (event) => {
+  const getId = event.id;
+  const links = document.querySelectorAll(".work-category button");
+  for (i = 0; i < links.length; i++) {
+    if (links[i].hasAttribute("class")) {
+      links[i].classList.remove("active");
+    }
+  }
+
+  event.classList.add("active");
+  const getCategory = document.querySelector(`.category-${getId}`);
+  const categories = document.querySelectorAll('div[class^="category-"]');
+  for (i = 0; i < categories.length; i++) {
+    if (categories[i].hasAttribute("class")) {
+      categories[i].classList.remove("showCategory");
+      categories[i].classList.add("hideCategory");
+    }
+  }
+
+  getCategory.classList.remove("hideCategory");
+  getCategory.classList.add("showCategory");
+};
+
+
